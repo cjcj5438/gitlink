bc493cbcf151fd1648288ebc2a310713c1143c5f
cjcj5438 <1002931123@qq.com>
2019-08-08 16:20:17 +0800

    123

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+PROJECT_DIR = 'path-to-your-project'
+ 
+echo 'start'
+# cd $PROJECT_DIR
+ 
+echo 'pull code'
+# git reset --hard origin/master && git clean -f
+# git pull && git checkout master
+ 
+# echo 'run npm script'
+# npm run build
+ 
+echo 'finished'
